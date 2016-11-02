4af81aa28f2d32779c54e59a53a0770661d149f3
slamma009 <alsnurb@gmail.com>
2016-11-01 22:33:53 -0500

    Grunt commit

---
+++ Eve-Assets/Eve-Assets/gruntfile.js
@@ -0,0 +1,28 @@
+﻿module.exports = function (grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            js: {
+                files: ['App/**/*'],
+                tasks: ['includeSource']
+            }
+        },
+        includeSource: {
+            options: {
+                basePath: 'App',
+                baseUrl: '/'
+            },
+            myTarget: {
+                files: {
+                    'Index.html': 'App/Index-before.html'
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-include-source');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['includeSource']);
+}
